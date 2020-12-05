ddf58b24f37952eba0b9261d570e73e7a04fe625
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-12-05 13:01:25 +0300

    declare remove task function

--- 31 Task Manager/scripts.js
+++ 31 Task Manager/scripts.js
@@ -34,21 +34,35 @@ function addTaskToDom () {
    // console.log(tasks);
     const taskList =  document.getElementById("taskList"); 
     taskList.innerHTML = tasks.map((task, i) => {
-        // id and label should be the same. Its how you link them together
         return `<li class="list"> 
         <div class="jumbotron"> 
+       
         <p> <span class="label label-danger"> ${task.done === true ? "done" : "not completed"} </span></p>
-        <div class="task"> <h2> <span style="font-weight:bold;"> Task : </span> ${task.taskName} </h2>  <p style="float:right"><span class="glyphicon glyphicon-user"></span> ${task.assigned} </p>
-        <h6> Task ID: ${task.id} </h6> </div>
-        <h5git> <span style="font-weight:bold;"> Description: </span>  ${task.description} </h3>
-        <p><span class="glyphicon glyphicon-time"></span>${task.deadline} </p>
+        
+        <div class="task"><h6> Task ID: ${task.id} </h6> <h2> <span style="font-weight:bold;"> Task : </span> ${task.taskName} </h2> 
+        <h5> <span style="font-weight:bold;"> Description: </span>  ${task.description} </h3>
+         </div>
+        <p><span class="glyphicon glyphicon-time"></span>  ${task.deadline} </p>
         <button type="button" class="rm-btn btn btn-danger" data-index=${i}> Cancel Task </button> 
         <button type="button" class="done-btn btn btn-success" data-index=${i}> Done </button> 
+        <p style="float:right"><span class="glyphicon glyphicon-user"></span> ${task.assigned} </p>
         </div>
         </li>`; 
       }).join('')
 
 }
-// event listener
-addTaskToDom ()
+
+function removeTask (e) {
+    if(e.target.classList[0] !== 'rm-btn') return;
+    // console.log(e.target.dataset);
+    const index = e.target.dataset.index;
+    tasks.splice(index,1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    addTaskToDom()
+}
+
+
+// event listeners
 form.addEventListener("submit",addNewTask);
+taskList.addEventListener("click",removeTask)
+addTaskToDom ()
